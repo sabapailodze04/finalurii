22cfc2914b68221ad38098b75ea1bdf68c88e6ba
sabapailodze04 <sabapailodze2004@gmail.com>
2025-07-27 06:17:48 -0700

    feature

---
+++ script.js
@@ -0,0 +1,37 @@
+let jokeDiv = document.getElementById("joke");
+let fetchButton = document.getElementById("fetchButton");
+let thumbsUpButton = document.getElementById("thumbsUpButton");
+let thumbsDownButton = document.getElementById("thumbsDownButton");
+let counterDiv = document.getElementById("likeDislikeCounter");
+
+let likeCount = 0;
+
+function updateCounter() {
+  counterDiv.textContent = "total Like/Dislike : " + likeCount;
+}
+
+function getJoke() {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      jokeDiv.textContent = data.value;
+    });
+}
+
+function increaseLike() {
+  likeCount++;
+  updateCounter();
+}
+
+function decreaseLike() {
+  if (likeCount > 0) {
+    likeCount--;
+    updateCounter();
+  }
+}
+
+fetchButton.addEventListener("click", getJoke);
+thumbsUpButton.addEventListener("click", increaseLike);
+thumbsDownButton.addEventListener("click", decreaseLike);
